f7ad1003523d98792ef013f3799a40714430edc1
SamuelmdLow <samuel.m.d.low@gmail.com>
2026-09-22 06:12:00 -0600

    Add meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,8 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>TubbDoose</title>
+	<meta name="description" content="TubbDoose.ca is the personal website of Sam Low." />
 </svelte:head>
 
 {@render children()}
